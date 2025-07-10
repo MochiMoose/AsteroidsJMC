08b4ce653c94a9bb4e81e927b4ce74a732fb6588
MochiMoose <wgratton1@myjmc.edu.au>
2025-07-10 13:29:04 +1000

    Screenwrap
    Particles also fixed.

--- Assets/Scripts/Screenwrap.cs
+++ Assets/Scripts/Screenwrap.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Screenwrap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private SpriteRenderer SpriteRenderer;
+    private bool HasBeenVisible = false;
+
+    private void Awake()
     {
-        
+        SpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (HasBeenVisible == false && SpriteRenderer.isVisible)
+        {
+            HasBeenVisible = true;
+        }
+        if (HasBeenVisible == false) 
+        {
+            return;
+        }
+
+        Vector2 screenPos =
+             Camera.main.WorldToScreenPoint(transform.position);
+
+        Vector2 newScreenPos = screenPos;
+
+        if (screenPos.x < 0)
+        {
+            newScreenPos.x = Screen.width;
+        }
+        else if (screenPos.x > Screen.width)
+        {
+            newScreenPos.x = 0;
+        }
+
+        if (screenPos.y < 0)
+        {
+            newScreenPos.y = Screen.height;
+        }
+        else if (screenPos.y > Screen.height)
+        {
+            newScreenPos.y = 0;
+        }
+
+        if (newScreenPos != screenPos)
+        {
+            Vector2 newWorldPos = Camera.main.ScreenToWorldPoint(newScreenPos);
+            transform.position = newWorldPos;
+        }
     }
 }
